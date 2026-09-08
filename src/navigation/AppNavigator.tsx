8573accd8da0979b776f0699833c65e358a13e38
abcp8844-source <abcp8844@gmail.com>
2026-09-08 23:04:17 +0700

    Create AppNavigator.tsx

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { RegisterScreen } from '../screens/auth/RegisterScreen';
+import { ForgotPasswordScreen } from '../screens/auth/ForgotPasswordScreen';
+import { MainTabs } from './MainTabs';
+
+const Stack = createNativeStackNavigator();
+
+interface AppNavigatorProps {
+  session: any;
+}
+
+export const AppNavigator: React.FC<AppNavigatorProps> = ({ session }) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {session && session.user ? (
+          <Stack.Screen name="MainTabs" component={MainTabs} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
