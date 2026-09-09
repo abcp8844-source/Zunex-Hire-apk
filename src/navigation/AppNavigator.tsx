335dab30670153c69da4a4367e209c41b4a9b937
abcp8844-source <abcp8844@gmail.com>
2026-09-09 16:42:37 +0700

    Update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -8,6 +8,14 @@ import { ForgotPasswordScreen } from '../screens/auth/ForgotPasswordScreen';
 import { MainTabs } from './MainTabs';
 
 import { ProfileScreen } from '../screens/profile/ProfileScreen';
+import { EditProfileScreen } from '../screens/profile/EditProfileScreen';
+import { PersonalDetailsScreen } from '../screens/profile/PersonalDetailsScreen';
+import { UserAboutScreen } from '../screens/profile/UserAboutScreen';
+import { FriendsListScreen } from '../screens/profile/FriendsListScreen';
+import { FindFriendsScreen } from '../screens/profile/FindFriendsScreen';
+import { ProfilePhotosScreen } from '../screens/profile/ProfilePhotosScreen';
+import { FriendRequestsScreen } from '../screens/profile/FriendRequestsScreen';
+
 import { SettingsScreen } from '../screens/menu/SettingsScreen';
 import { ActivityLogScreen } from '../screens/menu/ActivityLogScreen';
 import { BlockedUsersScreen } from '../screens/menu/BlockedUsersScreen';
@@ -28,6 +36,13 @@ export const AppNavigator: React.FC<AppNavigatorProps> = ({ session }) => {
           <>
             <Stack.Screen name="MainTabs" component={MainTabs} />
             <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+            <Stack.Screen name="PersonalDetails" component={PersonalDetailsScreen} />
+            <Stack.Screen name="UserAbout" component={UserAboutScreen} />
+            <Stack.Screen name="FriendsList" component={FriendsListScreen} />
+            <Stack.Screen name="FindFriends" component={FindFriendsScreen} />
+            <Stack.Screen name="ProfilePhotos" component={ProfilePhotosScreen} />
+            <Stack.Screen name="FriendRequests" component={FriendRequestsScreen} />
             <Stack.Screen name="Settings" component={SettingsScreen} />
             <Stack.Screen name="ActivityLog" component={ActivityLogScreen} />
             <Stack.Screen name="BlockedUsers" component={BlockedUsersScreen} />
