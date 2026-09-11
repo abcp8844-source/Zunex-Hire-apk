c60301d4dd8d5a0ef351d109926a8ba0a9c14a80
abcp8844-source <abcp8844@gmail.com>
2026-09-11 16:48:55 +0700

    Create reactions.ts

---
+++ src/constants/reactions.ts
@@ -0,0 +1,10 @@
+// constants/reactions.ts
+export const FB_REACTIONS: Record<string, { label: string; icon: string }> = {
+  like: { label: 'Like', icon: 'https://raw.githubusercontent.com/facebook/react-native/main/packages/rn-tester/js/assets/like.png' },
+  love: { label: 'Love', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02a.png' },
+  care: { label: 'Care', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02e.png' },
+  haha: { label: 'Haha', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02b.png' },
+  wow: { label: 'Wow', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02c.png' },
+  sad: { label: 'Sad', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02d.png' },
+  angry: { label: 'Angry', icon: 'https://images.rawpixel.com/image_png_800/2022/10/rm378-02f.png' },
+};
